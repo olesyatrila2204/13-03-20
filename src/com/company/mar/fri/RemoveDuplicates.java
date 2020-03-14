412d19e40c864343fb8e7061491f1a930864a14f
Olesya Trila <olesyatrila@Olesyas-MacBook-Pro.local>
2020-03-13 20:58:23 -0500

    mar 13 commit

---
+++ src/com/company/mar/fri/RemoveDuplicates.java
@@ -0,0 +1,5 @@
+package com.company.mar.fri;
+
+public class RemoveDuplicates {
+
+}
---
+++ src/com/company/mar/fri/StringWork.java
@@ -0,0 +1,28 @@
+package com.company.mar.fri;
+
+
+import java.util.List;
+
+/**
+ *
+ *
+ */
+public class StringWork {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Return capitalized words. Remove punctuation if exists
+     *
+     * Ex: "hello, my name is olesya. I am very smart"
+     *
+     *
+     * @param text
+     * @return
+     */
+    public String[] capitalize(String text) {
+        return null;
+    }
+}
